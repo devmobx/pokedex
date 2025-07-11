c6101a24c8d5f72f1dfa912b2b26de5390dc6ee7
Craig Chick <craig.chick.ext@up.coop>
2025-07-12 01:37:26 +0200

    Fixed tinit color on back button

--- src/app/main/pokemon/_layout.tsx
+++ src/app/main/pokemon/_layout.tsx
@@ -2,13 +2,18 @@ import { Stack } from "expo-router";
 import "react-native-reanimated";
 
 import { useHeaderConfig } from "@/navigation";
+import { useTheme } from "@/theme";
 
 export default function PokemonLayout() {
   const headerConfig = useHeaderConfig();
+  const theme = useTheme();
 
   return (
     <Stack screenOptions={headerConfig}>
-      <Stack.Screen name="details" />
+      <Stack.Screen
+        name="details"
+        options={{ headerTintColor: theme.color.white }}
+      />
     </Stack>
   );
 }
--- src/navigation/components/HeaderLeftButton.tsx
+++ src/navigation/components/HeaderLeftButton.tsx
@@ -3,23 +3,20 @@ import { useRouter } from "expo-router";
 
 import BackArrow from "@/assets/svg/back_arrow.svg";
 import { AnimatedPressable, Graphic } from "@/components/shared";
-import { useTheme } from "@/theme";
 
 export const HeaderLeftButton = (props: NativeStackHeaderLeftProps) => {
-  const theme = useTheme();
   const router = useRouter();
 
   return (
     <AnimatedPressable
       onPress={() => props.canGoBack && router.back()}
-      backgroundColor="transparentGrey1"
       width={30}
       height={30}
       justifyContent="center"
       alignItems="center"
       borderRadius="full"
     >
-      <Graphic as={BackArrow} color={theme.color.contrast} />
+      <Graphic as={BackArrow} color={props.tintColor} />
     </AnimatedPressable>
   );
 };
